ec79757a4be1108dd013764fdafb714eb365da83
Luis Galindo <egalindo54@uabc.edu.mx>
2021-10-12 11:55:14 -0600

    mar 12 oct 2021 11:55:31

--- Find-pi-to-the-nth-digit.rkt
+++ Find-pi-to-the-nth-digit.rkt
@@ -1,10 +1,20 @@
 
 #lang racket/gui
 
-(define (leibniz n)                     ;(expt 10 x)
+;; https://es.wikipedia.org/wiki/Serie_de_Leibniz
+(define (leibniz n)
   (if (not (= n -1))
       (+ (/ (expt -1 n) (+ (* 2 n) 1)) (leibniz (sub1 n)))
       0 ))
 
-(define (calc-pi x)
+;; https://es.wikipedia.org/wiki/Producto_de_Wallis
+(define (wallis n)
+  (if (not (= n 0))
+      (*  (/ (* 4 (expt n 2))  (- (* 4 (expt n 2)) 1))  (wallis (sub1 n)))
+      1 ))
+
+(define (leibniz-pi x)
   (* 4 (leibniz (expt 10 x))))
+
+(define (wallis-pi n)
+  (* 2 (wallis (expt 10 n) )) )
