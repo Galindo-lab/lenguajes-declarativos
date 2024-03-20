926d2e6f3788e88ba6f5efc3df032ab1e9bd0927
Luis E. Galindo <78140218+Galindo-lab@users.noreply.github.com>
2024-03-20 12:04:27 -0700

    Add files via upload

--- Mundo de bloques.pl
+++ Mundo de bloques.pl
@@ -1,67 +1,44 @@
 % Mundo de bloques
-
 % ejemplo consulta: resolver([3,4,1,2],[1,2,3,4]).
 
-print_list([]) :- nl.
+print_list([]) :- nl, !.
 print_list([X|Xs]) :- write(X), write(' '), print_list(Xs).
 
-conc([],L,L).
+conc([],L,L) :- !.
 conc([X|R],L,[X|Q]):- conc(R,L,Q).
 
-inverso([],[]).
+inverso([],[]) :- !.
 inverso([X|R], L) :- inverso(R, Q), conc(Q, [X], L).
 
-resolver(A, Z) :- inverso(Z, L), solucion([A,[],[]], L).
+elemento(X,[X|_]) :- !.
+elemento(X,[_|R]) :- elemento(X,R).
 
-solucion(_, []).
+resolver(A, Z) :-
+    inverso(Z, L),
+    print_list([A,[],[]]),
+    solucion([A,[],[]], L).
+
+solucion(_, []) :- !.           % si siguiente esta vacio 
 
 solucion([[A|R],B,C], [A|T]) :-
+    % si el primer elemento de 'A' es el siguiente
     print_list([R,B,[A|C]]),
     solucion([R,B,[A|C]], T).
 
 solucion([A,[B|R],C], [B|T]) :-
+    % si el primer elemento de 'B' es el siguiente
     print_list([A,R,[B|C]]),
     solucion([A,R,[B|C]], T).
 
-solucion([[A|R],B,C], Z) :-
-    print_list([R,[A|B],C]),
-    solucion([R,[A|B],C], Z).
+solucion([[A|As],B,C], [Z|R]) :-
+    % si siguiente esta el la pila A
+    elemento(Z, As),
+    print_list([As,[A|B],C]),
+    solucion([As,[A|B],C], [Z|R]).
 
-solucion([[],[B|R],C], Z) :-
-    print_list([[B], R, C]),
-    solucion([[B], R, C], Z).
+solucion([A,[B|Bs],C], [Z|R]) :-
+    % si el siguiente elemento esta en la pila B
+    elemento(Z, Bs),
+    print_list([[B|A],Bs,C]),
+    solucion([[B|A],Bs,C], [Z|R]).
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% ultimo_elemento([X], X).
-
-%% ultimo_elemento([_|T], X) :-
-%%     ultimo_elemento(T, X).
-
-%% solucion([[],[],A], A).
-
-%% solucion([[A|R],B,C], Z) :-
-%%     not(ultimo_elemento(Z, A)),
-%%     solucion([R,[A|B],C], Z).
-
-%% solucion([[A|R],B,C], Z) :-
-%%     ultimo_elemento(Z, A),
-%%     solucion([R,B,[A|C]], Z).
-
-%% solucion([[],A,B], Z):-
-%%     solucion([A,[],B], Z).
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            
-%% % get_pos(Elemento, Lista, Indice).
-%% % siempre debe contener el elemento
-
-%% get_ind(E, [X|_], 0) :- E = X.
-%% get_ind(E, [X|R], I) :- E =\= X, get_ind(E, R, Q), I is Q+1.
-
-%% %% solucion([[A|Q],[],[]], [[],[],B]) :-
-%% %%     get_ind(A, B, Indice),
-%% %%     Indice = 2,
-%% %%     solucion([Q,[],[A]], [[],[],B]).
